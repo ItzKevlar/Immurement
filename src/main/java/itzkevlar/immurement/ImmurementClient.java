82e5a56a2acee23247a3010a60b2b2335bd25373
ItzKevlar <ghostlymms082010@gmail.com>
2025-11-16 22:54:31 -0500

    1.21.1

---
+++ src/main/java/itzkevlar/immurement/ImmurementClient.java
@@ -0,0 +1,10 @@
+package itzkevlar.immurement;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImmurementClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
